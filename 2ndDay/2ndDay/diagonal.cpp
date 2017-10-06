439f12f258f588f40bad92fc9fd533e56ddcf8d9
Dahae Kim <dh77dh77@gmail.com>
2017-10-07 01:16:30 +0900

    3049 diagonal

---
+++ 2ndDay/2ndDay/diagonal.cpp
@@ -0,0 +1,24 @@
+// ¹éºÐ 3049¹ø
+
+#include <iostream>
+using namespace std;
+
+int Calculate(int input) {
+
+	int result;
+	result = ((input) * (input - 1) * (input - 2) * (input - 3)) / (4 * 3 * 2 * 1);
+	return result;
+}
+
+
+int main() {
+
+	int input;
+	cin >> input;
+	if (input < 4)
+		cout << "0" << endl;
+	else {
+		cout << Calculate(input) << endl;
+	}
+
+}
