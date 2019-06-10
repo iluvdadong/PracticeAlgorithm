cbc5d9f2962f56701b4c9bb05fc105ce78a8a38b
Dahae Kim <dh77dh77@gmail.com>
2019-06-10 21:50:48 +0900

    입출력

---
+++ basicProblems_100/basicProblems_100/basicProblems_100.cpp
@@ -0,0 +1,15 @@
+﻿//PracticeAlgorithm BasicProblems_100
+//190610: 01~11 완료 
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+	
+	string a;
+	std:cin >> a;
+	std::cout << a << std::endl;
+
+	return 0;
+}
