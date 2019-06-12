776d3efb94885492d03d40772ddeb6942acb7084
Dahae Kim <dh77dh77@gmail.com>
2019-06-12 12:44:41 +0900

    dice

--- basicProblems_100/basicProblems_100/basicProblems_100.cpp
+++ basicProblems_100/basicProblems_100/basicProblems_100.cpp
@@ -1,15 +1,38 @@
-﻿//PracticeAlgorithm BasicProblems_100
+﻿//practicealgorithm basicproblems_100
 //190610: 01~11 완료 
 
-#include <iostream>
-using namespace std;
-#include <string>
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//
+//int main() {
+//	
+//	string a;
+//	std:cin >> a;
+//	std::cout << a << std::endl;
+//
+//	return 0;
+//}
+//
+// 6면 주사위 100번 굴려서 나온 각 면의수를 배열에 저장하여 출력
 
-int main() {
-	
-	string a;
-	std:cin >> a;
-	std::cout << a << std::endl;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-	return 0;
+void main() {
+
+	int hist[6] = { 0, }; // 배열로 6개 주사위 눈금만큼 공간 할당
+	int n, i = 0; //상수 초기화
+	srand(time(null)); //랜덤함수 생성
+
+	do {
+		i++;
+		n = rand() % 6 + 1; // rand()%6 + 1 -> 1~6까지 숫자 나옴
+		hist[n - 1] += 1;
+	} while (i < 100);
+
+	for (i = 0; i < 6; i++)
+		print("[%d] = %d\n", i + 1, hist[i])
+
 }
