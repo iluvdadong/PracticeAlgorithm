86e5960ca11b22042d9fdaaabc30b6821817cdd5
Dahae Kim <dh77dh77@gmail.com>
2019-08-25 20:25:58 +0900

    dfs bfs practice start

---
+++ AlgorithmStudy_BJ/AlgorithmStudy_BJ/D3_01.cpp
@@ -0,0 +1,18 @@
+//D3 - »ï¼ºSW EXPERT È¸¹®1
+
+#include <stdio.h>
+char a[9][9], n;
+
+int main() {
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			scanf("%d", a[i][j]);
+		}
+	}
+
+	
+
+}
