0f9ffaca23784d1994fda7ff205a714f75b47049
Dahae Kim <dh77dh77@gmail.com>
2019-08-28 12:08:19 +0900

    점수계산 기본코딩

---
+++ PS_Practice90/PS_Practice90/theCoder01.cpp
@@ -0,0 +1,31 @@
+// The Coder 01 - 점수계산(기본코딩)
+// 완료
+
+#include <stdio.h>
+
+int main() {
+
+	int n, a[101], check = 0, res =0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 1; i <= n; i++) {
+	
+		if (a[i] == 1 && check == 0) {
+			check++;
+			res = res + 1;
+		}
+		else if (a[i] == 1 && check > 0) {
+			check++;
+			res = res + check;
+		}
+		else {
+			check = 0;
+		}
+	}
+	printf("%d", res);
+	return 0;
+}
